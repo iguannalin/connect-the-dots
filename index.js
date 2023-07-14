d0c3a3d79d95d290906f912c025bf2bcca7ac360
seasons986 <annalin12345@gmail.com>
2023-07-14 14:32:01 -0400

    erasing

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
+  function connectWord(e) {
+    e.preventDefault();
+    e.target.classList.remove("grayed")
+  }
+
   function getLines(t) {
     let div = document.getElementById("poem");
     let wmax = 350;
     let r = getRandomInt(wmax, t.length);
     let line = t.substring(r-wmax, r);
-    console.log(line)
+    // console.log(line)
     line.split(" ").forEach((word, index) => {
       if (index < wmax) {
         lines.push(word);
-        div.innerHTML += (word + " ");
+        let span = document.createElement("a");
+        span.innerHTML = (word.trim() + " ");
+        span.classList = "grayed";
+        span.onclick = connectWord;
+        div.appendChild(span);
       }
     })
   }
@@ -26,7 +35,7 @@ window.addEventListener("load", () => {
   fetch("waves.txt").then((file) => file.text()).then((d) => {
     text = d;
     getLines(text);
-    console.log(text.length);
+    // console.log(text.length);
     // call fx here
   });
 });
